592e45bc0b1a9e0976e928dc276ad96a5fc21553
leankkk <leankonito@gmail.com>
2026-08-12 14:17:45 -0300

    empezando con los tipos de mesa en tipos.ts esta la forma en ese archivo

---
+++ Front-end/src/data/tipos.ts
@@ -0,0 +1,11 @@
+// las formas de los datos que uso  en el proyecto(quedamos con tinchi)
+export type EstadoMesa = 'vacia' | 'ocupada' | 'llamando'
+export type Mesa = {
+  id: number
+  numero: number
+  x: number
+  y: number
+  forma: 'circular' | 'cuadrado'
+  estado: EstadoMesa
+}
+//number es basicamente aca va un numero, que seria el id, el numero de pesa, y la posicion. 
