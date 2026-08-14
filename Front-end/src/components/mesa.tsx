79c3a2e019ab68e5ce497843579a2145daa10495
leankkk <leankonito@gmail.com>
2026-08-14 09:01:45 -0300

    creado mesa.tsx y css y constantes ts creado con tamaño de la mesa

---
+++ Front-end/src/components/mesa.tsx
@@ -0,0 +1,26 @@
+import type { Mesa as DatosMesa } from '../data/tipos'
+import { TAMAÑO_MESA } from '../data/constantes'
+import './Mesa.css'
+
+
+// le pongo otro nombre al tipo porque el componente ya se llama Mesa
+type Props = {
+  mesa: DatosMesa
+}
+
+
+export default function Mesa({ mesa }: Props) {
+  return (
+    <div
+      className={'mesa mesa-' + mesa.forma + ' mesa-' + mesa.estado}
+      style={{
+        left: mesa.x,
+        top: mesa.y,
+        width: TAMAÑO_MESA,
+        height: TAMAÑO_MESA,
+      }}
+    >
+      {mesa.numero}
+    </div>
+  )
+}
