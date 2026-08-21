f0f19acce307c438ebe5cd4889e3f69c236e5e37
TINCHITO123 <bs2893@est.ort.edu.ar>
2026-08-21 14:29:52 -0300

    conexion server

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,12 +1,12 @@
-// Paso 2 (esto va en CADA archivo que use express)
-const express = require('express');
+  // Paso 2 (esto va en CADA archivo que use express)
+  const express = require('express');
 
-const app = express();
+  const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola SOLI');
-});
+  app.get('/', (req, res) => {
+    res.send('Hola SOLI');
+  });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
-});
+  app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+  });
