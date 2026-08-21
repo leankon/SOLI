f941fe46d39a766a83b72dcaaa88a0d52833dded
TINCHITO123 <bs2893@est.ort.edu.ar>
2026-08-21 11:48:50 -0300

    Express + package

---
+++ Back-end/index.js
@@ -0,0 +1,12 @@
+// Paso 2 (esto va en CADA archivo que use express)
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola SOLI');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000');
+});
