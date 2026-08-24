523c21decfa9f23e03e3caee98be36055a05b95c
TINCHITO123 <bs2893@est.ort.edu.ar>
2026-08-24 08:39:22 -0300

    - db +.env Conección lista. Empiezo con rutas

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,12 +1,19 @@
-  // Paso 2 (esto va en CADA archivo que use express)
-  const express = require('express');
+import express from "express";
+import { pool } from "./db.js";
 
-  const app = express();
+const app = express();
 
-  app.get('/', (req, res) => {
-    res.send('Hola SOLI');
-  });
+app.get('/', (req, res) => {
+  res.send('Hola SOLI');
+});
 
-  app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-  });
+app.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000');
+});
+
+
+// Rutas________________________________________________________________________________
+
+
+  
+
