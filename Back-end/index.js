b2ee5c42b752b597b6f8b5d3d5b3cfe9f8945f9e
TINCHITO123 <bs2893@est.ort.edu.ar>
2026-08-24 10:23:12 -0300

    -NODE MODULES Chau .ENv

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,19 +1,22 @@
 import express from "express";
-import { pool } from "./db.js";
-
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hola SOLI');
+import artistas from "./controllers/artistas.js";
+import albumes from "./controllers/albumes.js";
+import canciones from "./controllers/canciones.js";
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("SpoTICfy API working!");
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+const server = app.listen(port, () => {
+  console.log(`SpoTICfy API listening at http://localhost:${port}`);
 });
+
+export { app, server };
 
 
 // Rutas________________________________________________________________________________
-
-
-  
-
