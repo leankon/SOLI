7d2aa7d83632078dd85b2eec8d2c15b34614626d
TINCHITO123 <bs2893@est.ort.edu.ar>
2026-09-14 08:23:52 -0300

    IMPLEMENTACIÓN CORS

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,4 +1,5 @@
   import express from "express";
+  import cors from "cors";
   const app = express();
   const port = 3000;
 
@@ -8,6 +9,7 @@
   import solicitudes from "./controllers/solicitud.js";
   import usuarios from "./controllers/usuario.js";
 
+  app.use(cors());
   app.use(express.json());
 
   app.get("/", (_, res) => {
