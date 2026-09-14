1810b6d9c831c8a45748d25c7a0d7ffe5d01f5f1
leankkk <leankonito@gmail.com>
2026-09-14 13:53:31 -0300

    agregando el get de catalogo

--- Front-end/src/data/constantes.ts
+++ Front-end/src/data/constantes.ts
@@ -1,3 +1,4 @@
 // el lado de una mesa en pixeles. lo uso en la vista mozo y en el editor de plano
 export const tamano_MESA = 160
 export const EMAIL_RESTAURANTE = 'encargado@gmail.com'
+export const API = 'http://localhost:3000'
